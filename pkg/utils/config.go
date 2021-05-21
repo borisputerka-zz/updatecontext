95147e78b2a2422c3f436f775f8d46ab3dbb47cd
Boris Puterka <boris.puterka@gmail.com>
2021-05-21 18:33:48 +0200

    fix: eks and gke cluster name

--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -2,12 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"regexp"
+
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
-	"strings"
+
 )
 
 type Config struct {
@@ -29,9 +31,22 @@ func (c *Config) Update() (err error) {
 }
 
 func (c *Config) ContextName(cluster string, namespace string) string {
-	clusterParts := strings.Split(cluster, "/")
-	cluster = clusterParts[len(clusterParts)-1]
-	return fmt.Sprintf("%s/%s", namespace, cluster)
+	gkeRegexp := "^gke_([^_\\n]*)_([^_\\n]*)_([^_\\n]*)$"
+	eksRegexp := "^arn:([^:\\n]*):([^:\\n]*):([^:\\n]*):([^:\\n]*):(([^:\\/\\n]*)[:\\/])?(.*)$"
+	if matched, _ := regexp.MatchString(eksRegexp, cluster); matched {
+		eksRe := regexp.MustCompile(eksRegexp)
+		eksClusterSubmatch := eksRe.FindStringSubmatch(cluster)
+		clusterName := eksClusterSubmatch[len(eksClusterSubmatch)-1]
+		return fmt.Sprintf("%s/%s", namespace, clusterName)
+
+	} else if matched, _ := regexp.MatchString(gkeRegexp, cluster); matched {
+		gkeRe := regexp.MustCompile(gkeRegexp)
+		gkeClusterSubmatch := gkeRe.FindStringSubmatch(cluster)
+		clusterName := gkeClusterSubmatch[len(gkeClusterSubmatch)-1]
+		return fmt.Sprintf("%s/%s", namespace, clusterName)
+	} else {
+		return fmt.Sprintf("%s/%s", namespace, cluster)
+	}
 }
 
 func (c *Config) DeleteContexts(contexts map[string]*api.Context) error {
